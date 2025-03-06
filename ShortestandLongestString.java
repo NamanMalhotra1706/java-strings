149c774dd70c58868b87f15449d58f2ea37befac
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-03-06 10:27:13 +0530

    [Naman Malhotra] ADD: Shortest and Longest in String

---
+++ ShortestandLongestString.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ShortestandLongestString {
+
+    // Method to split a given string into words using space as a delimiter
+    public static String[] splitWords(String str) {
+        String words[] = str.split(" ");
+        return words;
+    }
+
+    // Method to find the shortest and longest words in the given array of words
+    public static String[] findShortestandLongest(String[] words) {
+        String shortest = ""; 
+        String longest = ""; 
+
+        int shortestLength = Integer.MAX_VALUE; 
+        int longestLength = Integer.MIN_VALUE;  
+
+       
+        for (int i = 0; i < words.length; i++) {
+            if (shortestLength >= words[i].length()) {
+                shortestLength = words[i].length();
+                shortest = words[i];
+            }
+            else if (longestLength <= words[i].length()) { 
+                longestLength = words[i].length();
+                longest = words[i];
+            }
+        }
+
+        return new String[]{shortest, longest}; // Return the shortest and longest words
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+        System.out.print("Enter the string: ");
+        String str = sc.nextLine();
+
+        String[] words = splitWords(str);
+        String[] ShortestandLongest = findShortestandLongest(words); 
+        System.out.println(ShortestandLongest[0] + " " + ShortestandLongest[1]); 
+
+    }
+}
